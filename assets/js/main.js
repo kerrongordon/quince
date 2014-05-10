2e7993817e75d00e934182c1ac21e44c9e95a71c
kerrongordon <kgpsounds.com@gmail.com>
2014-05-10 16:49:32 -0400

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-      $('.menubutton').on('click', function() {
-          $('.moblemenu').toggleClass('showmenu');
+      $('.menubutton').click(function() {
+          $('nav.moblemenu').toggleClass('showmenu');
      });
     
     //init 
     $('#loadPage').load('pages/index.html');
     
+    //load menu page
     $('ul#navlist li a').click(function() {
         var pages = $(this).attr('href');
         $('#loadPage').load('pages/' + pages + '.html');
